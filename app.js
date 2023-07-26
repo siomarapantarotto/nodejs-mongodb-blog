c414b28e36a944013ecc823b5a35e7207382a9ed
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-07-26 14:09:24 -0300

    Register view engine and render() views with title

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ const express = require('express');
 // Express app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
 // Listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p><h1>Home Page</h1></p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    //res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index', {title: 'Homepage'});
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p><h1>About Page</h1></p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    //res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 });
 
 // Redirects
@@ -27,5 +32,6 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    //res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404 Not Found'});
 });
