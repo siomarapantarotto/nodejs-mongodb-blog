f5e85593cfdc704efe87f4f5f87b9a22d06b644e
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-07-28 01:09:11 -0300

    Set blogs array and render create new blog view

--- app.js
+++ app.js
@@ -14,10 +14,19 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Blog 1', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... 1'},
+        {title: 'Blog 2', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... 2'},
+        {title: 'Blog 3', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... 3'},
+    ];
     //res.send('<p><h1>Home Page</h1></p>');
     //res.sendFile('./views/index.html', {root: __dirname});
     res.render('index', {title: 'Homepage'});
 });
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
 
 app.get('/about', (req, res) => {
     //res.send('<p><h1>About Page</h1></p>');
