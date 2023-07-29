a0c77719c9d90c0ab979bce07ad73ce976c7a55d
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-07-29 15:56:50 -0300

    Install mongoose

--- app.js
+++ app.js
@@ -3,23 +3,27 @@
  */
 
 const express = require('express');
+const morgan = require('morgan');
 
 // Express app
 const app = express();
 
-// Register view engine
+// Register view engine to implement templates
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
 
 // Listen for requests
 app.listen(3000);
 
+// HTTP request logger middleware
+app.use(morgan('dev')); // logger middleware - log details of every request
+
 // Routing
 app.get('/', (req, res) => {
     const blogs = [
-      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Fall In Love With Nodejs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Why MongoDB Succeeds', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Javascript for Frontend and Backend', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     ];
     res.render('index', { title: 'Home', blogs });
   });
@@ -32,7 +36,7 @@ app.get('/', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
   });
   
-  // 404 page not found
+  // middleware to handle 404 error
   app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
+    res.status(404).render('404', { title: '404 Page not found' });
   });
