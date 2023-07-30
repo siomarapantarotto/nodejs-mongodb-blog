4e0a78a65d69b16ac212cbb9570f06ee0f508820
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-07-30 05:12:28 -0300

    Implement POST request to add new blog into DB

--- app.js
+++ app.js
@@ -38,28 +38,47 @@ app.set('view engine', 'ejs');
 
 // middleware & static files (CSS, images etc)
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended:true})); // middleware to pass data from forms
 app.use(morgan('dev')); // logger middleware - log details of every request
 
 
-// Routes
+// Basic routes
 app.get('/', (req, res) => {
-    const blogs = [
-      {title: 'Fall In Love With Nodejs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      {title: 'Why MongoDB Succeeds', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      {title: 'Javascript for Frontend and Backend', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
   });
 
   app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
   });
   
+  // Blog routes
+  app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 }) // display from the newest to the oldest
+      .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  app.post('/blogs', (req, res) => {
+    //console.log(req.body);
+    const blog = new Blog(req.body);
+    blog.save()
+      .then((result) => {
+        res.redirect('/blogs');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
   });
-  
 
+  
   // Middleware to handle 404 error
   app.use((req, res) => {
     res.status(404).render('404', { title: '404 Page not found' });
